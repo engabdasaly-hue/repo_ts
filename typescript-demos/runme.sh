9b336edcdecf25eb316e2da3bf1cc7c112e49b4b
abdullah-alassali <eng.abdasaly@gmail.com>
2026-09-03 13:31:35 +0200

    Update runme.sh

--- typescript-demos/runme.sh
+++ typescript-demos/runme.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+  
+GITHUB_TOKEN="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"
+
 curl -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: token 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" \
+  -H "Authorization: token $GITHUB_TOKEN" \
   https://api.github.com/repos/engabdasaly-hue/repo_ts/dispatches \
   -d '{"event_type":"run-tests","client_payload":{"env":"staging"}}'
+
+
