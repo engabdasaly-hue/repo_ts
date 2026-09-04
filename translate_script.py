385a9b5075174596d1a4704039a909028075d8fc
Alassali Abdullah <eng.abdasaly@gmail.com>
2026-09-04 10:22:17 +0200

    reset

--- translate_script.py
+++ translate_script.py
@@ -1,7 +1,7 @@
 from translate import Translator
 translator= Translator(to_lang="fr")
-with open("file4.txt","r") as f:
+with open("file1.txt","r") as f:
 	english_txt=f.read()
 french_txt=translator.translate(english_txt)
-with open("file5.txt","w",encoding="utf-8") as f:
+with open("file2.txt","w",encoding="utf-8") as f:
 	f.write(french_txt)
